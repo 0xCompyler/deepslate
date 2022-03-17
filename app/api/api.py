e08c9cffcee3addd9ed0ddcdc8bc4095c3dbe584
radioactive11 <roy.arijit2001@gmail.com>
2022-03-17 12:06:19 +0530

    :bookmark: `stable` comments sentiment working

--- app/api/api.py
+++ app/api/api.py
@@ -1,9 +1,11 @@
+import re
 from sys import prefix
 from fastapi import APIRouter
 
-from app.api.endpoints import transcribe, touch
+from app.api.endpoints import transcribe, touch, comments
 
 api_router = APIRouter()
 
 api_router.include_router(transcribe.router, prefix="/transcribe")
 api_router.include_router(touch.router, prefix="/touch")
+api_router.include_router(comments.router, prefix="/comments")
